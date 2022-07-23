87107f2a96ecfbe6d0028f41ec4fd1eac40825f9
Thibaud <thibaud.brault@protonmail.com>
2022-07-23 14:06:09 +0200

    small modif

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Head>
+        <title>PRBN | Pokémon Radio Broadcasting Network</title>
+        <meta name="description" content="Listen to every pokémon games ost" />
+      </Head>
+    </>
   )
 }
