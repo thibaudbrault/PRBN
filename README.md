6c1f15eacc473c760f5e0ca8622683eee8bd8e32
Thibaud <thibaud.brault@protonmail.com>
2022-07-23 18:56:22 +0200

    added react icons

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 <h1 align="center">
     PRBN
 </h1>
-<br />
+
+---
+
 <h2 align="center">
     Pokémon Radio Broadcasting Network
 </h2>
--- components/Player/Player.js
+++ components/Player/Player.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import musics from '../../helpers/musics.json';
 import { Main } from './Styled.Player';
 
+import {TbPlayerPlay, TbPlayerPause, TbPlayerSkipBack, TbPlayerSkipForward} from 'react-icons/tb';
+
 export default function Player() {
-  return (
-    <Main>
-        <audio src={musics[0].link} preload='metadata'></audio>
-        <button>-10s</button>
-        <button>play</button>
-        <button>pause</button>
-        <button>+10s</button>
-        <div>
-            <p>0:00</p>
-            <input type="range" />
-            <p>2:50</p>
-        </div>
-    </Main>
-  )
+
+    const[isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying)
+    }
+
+    return (
+        <Main>
+            <div>
+                <p>0:00</p>
+                <input type="range" />
+                <p>2:50</p>
+            </div>
+            <div>
+                <audio src={musics[0].link} preload='metadata'></audio>
+                <button title='Back 10 seconds'><TbPlayerSkipBack /></button>
+                <button onClick={togglePlayPause}>
+                    {isPlaying ? (
+                        <TbPlayerPause />
+                    ) : (
+                        <TbPlayerPlay />
+                    )}
+                </button>
+                <button title='Forward 10 seconds'><TbPlayerSkipForward /></button>
+            </div>
+        </Main>
+    )
 }
